f9e635f4824e274a846d67edff2b0f862597f0c2
Matthew Dumont <mjdumont1@gmail.com>
2011-09-14 09:49:28 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpringConference2012::Application.config.secret_token = 'b85f8a8aedc7d297c1fbc2cbdc0f23c70c351b2d0fc34d71488cdece496491a14fb2a8402b5e0aa1ede681cca96ce99c18bf48a1b4f0d27dd9d27c300c4670ed'
